503d4e22e8247bcd18cc12cc10a1ba05ab0f7cf8
rajendrakrshaw <rajendra250601@gmail.com>
2023-01-23 23:46:19 +0530

    bug fixed

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,9 @@
 import './style.css';
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+  // import * as THREE from 'https://unpkg.com/three@<version>/build/three.module.js';
+  // import { OrbitControls } from 'https://unpkg.com/three@<version>/examples/jsm/controls/OrbitControls.js';
 
 // Setup
 
